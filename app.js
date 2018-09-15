e9cef9bdb5b40fd4453b504ddcfa0d5948c51ccb
akhamido <akobirkhamidov@gmail.com>
2018-09-15 13:46:05 -0700

    a

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(function(req, res, next){
 app.use(indexRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
-// app.listen(process.env.PORT, process.env.IP, function(){
-app.listen(3000, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
+//app.listen(3000, function(){
     console.log("The YelpCamp Server has started!");
 });
